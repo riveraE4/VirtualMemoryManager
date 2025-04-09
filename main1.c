8ab79ae34e50347aeb4a111a3fb7f2bac1002a57
Eric Rivera <ericrivera@Erics-MacBook-Pro-6.local>
2025-04-09 10:24:09 -0400

    starter code for main1.c

---
+++ main1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define PAGE_SIZE 256
+#define NUM_PAGES 256
+#define NUM_FRAMES 256
+#define TLB_SIZE 16
+
+typdef struct {
+  int page;
+  int frame;
+} TLB_Entry;
+
+int main(int argc, char** argv)
+{
+  
+  return 0;
+}
