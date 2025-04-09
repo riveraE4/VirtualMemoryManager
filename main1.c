901909a3c412c7eab378d46079c9ebc2e28fd3c6
Eric Rivera <ericrivera@Erics-MacBook-Pro-6.local>
2025-04-09 10:36:07 -0400

    file opening and outputting to txt

--- main1.c
+++ main1.c
@@ -10,10 +10,28 @@
 typdef struct {
   int page;
   int frame;
-} TLB_Entry;
+} TLBEntry;
 
 int main(int argc, char** argv)
 {
-  
+  FILE *addressFile = fopen(argv[1], "r");
+  if (addressFile == NULL) {
+    perror("Can't open file");
+    exit(1);
+  }
+
+  FILE *backingStore = fopen("BACKING_STORE.bin", "rb");
+  if (backingStore == NULL) {
+    perror("Can't open .bin");
+    exit(1);
+  }
+
+  // writes the output of out1 - out3 to a .txt file
+  FILE *out1 = fopen("out1.txt", "w"); 
+  FILE *out2 = fopen("out2.txt", "w");
+  FILE *out3 = fopen("out3.txt", "w");
+
+
+
   return 0;
 }
