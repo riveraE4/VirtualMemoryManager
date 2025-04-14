0763ee6a2ec04c771edc9383991ebee86a76cd88
Eric Rivera <ericrivera@erics-mbp-6.lan>
2025-04-13 21:18:13 -0400

    final code for all three

--- main1.c
+++ main1.c
@@ -13,34 +13,17 @@ typedef struct {
 } TLBEntry;
 
 int main(int argc, char** argv) {
-    /* error checking */
-    if (argc < 2) {
-        fprintf(stderr, "Usage: %s <address_file>\n", argv[0]);
-        exit(1);
-    }
-
+    
     /* open the address file for reading logical addresses */
     FILE *addressFile = fopen(argv[1], "r");
-    if (addressFile == NULL) {
-        perror("can't open file");
-        exit(1);
-    }
 
     /* open the backing store binary file */
     FILE *backingStore = fopen("BACKING_STORE.bin", "rb");
-    if (backingStore == NULL) {
-        perror("can't open .bin");
-        exit(1);
-    }
 
     /* open output files for writing results */
     FILE *out1 = fopen("out1.txt", "w"); /* logical addresses */
     FILE *out2 = fopen("out2.txt", "w"); /* physical addresses */
     FILE *out3 = fopen("out3.txt", "w"); /* values stored in physical memory */
-    if (!out1 || !out2 || !out3) { /* error checking, check to see if they can be opened */
-        perror("can't open output .txt file(s)");
-        exit(1);
-    }
 
     /* declare variables for address translation */
     int logicalAddress = 0;   
